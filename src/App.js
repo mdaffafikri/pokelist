8c850e209338255b43df4a24e6e49ce1056bf97f
daps <daffafikri@ymail.com>
2021-03-06 09:46:58 +0700

    Change the table to material-ui table

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
 // import logo from './logo.svg';
 import "./App.css";
+import * as Mui from "@material-ui/core";
+import * as Muicon from "@material-ui/icons";
 import Content from "./pages/Content";
 
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 
 require("bootstrap");
-
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
         <a className="navbar-brand" href="/#">
           PokeList
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">          
+          <ul className="navbar-nav ml-auto">
+           <li className="nav-item active">
               <a className="nav-link" href="/#">
                 Pokemon
-              </a>         
-            </li>            
+              </a>
+            </li>
             <li className="nav-item">
               <a className="nav-link" href="/#">
                 Region
-              </a>         
-            </li>            
+              </a>
+            </li>
             <li className="nav-item">
               <a className="nav-link" href="/#">
                 About
-              </a>         
-            </li>            
-          </ul>
+              </a>
+            </li></ul>
         </div>
       </nav>
-
 
       <div className="container mt-5">
         <Content></Content>
--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,105 +1,269 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from "react";
+import MaterialTable, { MTableToolbar } from "material-table";
+import * as Mui from "@material-ui/core";
 
 export default class Content extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            isLoaded : false,
-            next: '',
-            prev: null,
-            pokemons: [],            
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount(){      
-        document.title = 'PokeList'
-        
-        fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10`)
-        .then(data => data.json())
-        .then(json => {
-            this.setState({
-                isLoaded: true,
-                next: json.next,
-                previous: json.previous,
-                number: 1,
-                pokemons: json.results //result is contained in the json
-            })
-        })        
-    }
+    this.state = {
+      isLoaded: false,
+      next: "",
+      prev: null,
+      pokemons: [],
+      dialogueContent: "https://pokeapi.co/api/v2/pokemon/1/",
+    };
+  }
 
-    capitalize = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+  componentDidMount() {
+    document.title = "PokeList";
 
-    nextPage = (link) => {
-        fetch(link)
-        .then(data => data.json())
-        .then(json => {
-            this.setState({
-                isLoaded: true,
-                pokemons: json.results,
-                next: json.next,              
-                prev: json.previous,
-                number: this.state.number+10  
-            })
-        })
-    }
+    fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500`)
+      .then((data) => data.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          next: json.next,
+          previous: json.previous,
+          number: 1,
+          pokemons: json.results, //result is contained in the json
+        });
+      });
+  }
 
-    prevPage = (link) => {
-        fetch(link)
-        .then(data => data.json())
-        .then(json => {
-            this.setState({
-                isLoaded: true,
-                pokemons: json.results,
-                next: json.next,              
-                prev: json.previous,  
-                number: this.state.number-10
-            })
-        })
-    }
-    
-    render() {    
-        const {pokemons, isLoaded, next, prev, number} = this.state  //state
-        const {nextPage, prevPage, typeData} = this //function
-        return (
-            <div>
-                <h1>
-                    Database
-                </h1>
-                <table className="table table-bordered table-hover table-responsive w-100 d-block d-md-table">
-                    <thead className="thead-inverse thead-dark">
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {pokemons.map((item, i) =>                             
-                            <tr className="pointer" key={i}>
-                                <td>{i+number}</td>
-                                <td>{ this.capitalize(item.name)}</td>
-                                <td> <button id="view" className="btn btn-link text-danger view"><i className="fa fa-search" aria-hidden="true"></i>  </button> </td>
-                            </tr>                        
-                            )}                            
-                        </tbody>
-                </table>
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-                <div className="row justify-content-center d-flex">
-                    <div className="btn-group" role="group">                    
-                        <button disabled={(prev === null) ? true : false  }  className= "btn btn-danger" onClick={() => prevPage(prev)}>Prev</button>
-                                        
-                        <button className="btn btn-light" onClick={() => nextPage(next)}>Next</button>
-                    </div>
-                </div>
+  indexPoke = (string) => {
+    return this.state.pokemons.findIndex((obj) => obj.name === string);
+  };
 
-                <div id="tooltip" role="tooltip">
-                    My tooltip
-                    <div id="arrow" data-popper-arrow></div>
-                </div>
-            </div>            
-        )
-    }
+  nextPage = (link) => {
+    fetch(link)
+      .then((data) => data.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          pokemons: json.results,
+          next: json.next,
+          prev: json.previous,
+          number: this.state.number + 10,
+        });
+      });
+  };
+
+  prevPage = (link) => {
+    fetch(link)
+      .then((data) => data.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          pokemons: json.results,
+          next: json.next,
+          prev: json.previous,
+          number: this.state.number - 10,
+        });
+      });
+  };
+
+  changeContent = (link) => {
+    this.setState({ dialogueContent: link });
+  };
+
+  deleteRow = (index) => {
+    // if(confirm('Are u sure want to delete this row?') === true)
+    var array = [...this.state.pokemons]; // make a separate copy of the array    
+     if (index !== -1) {
+      array.splice(index, 1);
+      this.setState({pokemons: array});
+    }    
+  }
+
+  render() {
+    const { pokemons, isLoaded, next, prev, number } = this.state; //state
+    const { nextPage, prevPage, changeContent } = this; //function
+    return (
+      <div className="pokemonsTable">
+
+        <PopDialogue link={this.state.dialogueContent}></PopDialogue>
+
+        <MaterialTable
+          columns={[
+            { title: "Index", field: "number", render: (rowData) => this.indexPoke(rowData.name) + 1 },
+            { title: "Name", field: "name", render: (rowData) => this.capitalize(rowData.name) },
+            { title: "Action", field: "url",
+              render: (rowData) => (<>
+                <Mui.Tooltip title="Show Details" arrow>
+                  <button
+                    onClick={() => changeContent(rowData.url)}
+                    data-toggle="modal"
+                    data-target="#popDialogue"
+                    data-tip
+                    data-for="viewTip"
+                    className="btn btn-link text-danger view"
+                  >
+                    <i class="fa fa-info fa-lg" aria-hidden="true"></i>{" "}
+                  </button>
+                </Mui.Tooltip>        
+                <Mui.Tooltip title="Delete Data">                  
+                  <button className="btn btn-link text-danger" onClick={() => this.deleteRow(this.indexPoke(rowData.name)) }>
+                    <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
+                  </button>
+                </Mui.Tooltip>
+                </>        
+              ),
+            },            
+          ]}
+          data={pokemons}
+          title="Pokemon Database"
+        />
+      </div>
+    );
+  }
 }
+
+var PopDialogue = (props) => {
+  let [name, setName] = useState("");
+  let [type, setType] = useState([]);
+  let [sprite, setSprite] = useState(null);
+  let [stats, setStats] = useState(null);
+  let [loading, setLoading] = useState([]);
+
+  let capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  useEffect(() => {
+    let getData = () => {
+      setLoading(true);
+      fetch(props.link)
+        .then((data) => data.json())
+        .then((json) => {
+          setName(json.name);
+          setType(json.types);
+          setSprite(json.sprites);
+          setStats(json.stats);
+          setLoading(false);
+        });
+    };
+    getData();
+  }, [props.link]);
+
+  let rotateSprite = (front) => {
+    if (front) {
+      document.getElementById("sprite").src = sprite.front_default;
+    } else {
+      document.getElementById("sprite").src = sprite.back_default;
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="popDialogue"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">
+                Pokemon Detail
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body d-flex justify-content-around">
+              {loading ? (
+                <div className="spinner-border text-danger" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                <>
+                  <div>
+                    <button
+                      onClick={() => rotateSprite(true)}
+                      type="button"
+                      class="btn btn-link text-danger"
+                    >
+                      {" "}
+                      <i class="fa fa-undo fa-2x" aria-hidden="true"></i>{" "}
+                    </button>
+                    <img
+                      id="sprite"
+                      className="sprite"
+                      width="150"
+                      src={sprite.front_default}
+                      alt={sprite.front_default}
+                    />
+                    <button
+                      onClick={() => rotateSprite(false)}
+                      type="button"
+                      class="btn btn-link text-danger"
+                    >
+                      {" "}
+                      <i
+                        class="fa fa-undo fa-flip-horizontal fa-2x"
+                        aria-hidden="true"
+                      ></i>
+                    </button>
+                  </div>
+                  <div>
+                    <table className="table table-borderless table-sm text-left table-view">
+                      <tr>
+                        <td>Name</td>
+                        <td>:</td>
+                        <td>{capitalize(name)}</td>
+                      </tr>
+                      <tr>
+                        <td>Attack</td>
+                        <td>:</td>
+                        <td>{stats[1].base_stat}</td>
+                      </tr>
+                      <tr>
+                        <td>Defense</td>
+                        <td>:</td>
+                        <td>{stats[2].base_stat}</td>
+                      </tr>
+                      <tr>
+                        <td>Type</td>
+                        <td>:</td>
+                        <td>
+                          {type.map((item, index) => (
+                            <li className="list-unstyled" key={index}>
+                              {" "}
+                              {capitalize(item.type.name)}
+                            </li>
+                          ))}
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                class="btn btn-link text-danger"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
