6bd00e93d6229d254a19cd794e4df0e4295f3afb
daps <daffafikri@ymail.com>
2021-03-29 11:38:29 +0700

    Modfiy express server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const path = require('path');
+//server.js
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
-
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
+app.listen(port);
