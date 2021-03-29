132c32fb34fe589c002ce45cb6cd557552748807
daps <daffafikri@ymail.com>
2021-03-29 11:40:55 +0700

    revert express server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-//server.js
-const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
